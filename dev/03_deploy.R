8a3a4bd2ccd9ce734fa7dddb4da662e5b33a4242
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2022-10-17 17:13:19 +0200

    add build info.

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -4,4 +4,11 @@
 devtools::check()
 #rhub::check_for_cran()
 devtools::build()
-golem::add_dockerfile_with_renv_shinyproxy()
+#golem::add_dockerfile_with_renv_shinyproxy(output_dir = 'deploy/')
+out <- pkgbuild::build(
+  path = ".",
+  dest_path = 'deploy',
+  vignettes = FALSE,
+)
+# scp -r deploy/nmdtx_0.0.0.9000.tar.gz  tbrittoborges@shiny:apps/nmdtx/deploy/nmdtx_0.0.0.9000.tar.gz
+# docker build -f Dockerfile --progress=plain -t nmdtx:latest .
