c0ab36f55bcff12706f74d5947fa35015ed11079
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2022-10-20 13:18:52 +0200

    add code for dev

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -1,5 +1,22 @@
-options(golem.app.prod = FALSE, warn = 0)
+options(golem.app.prod = FALSE, warn = 1)
 options(shiny.port = httpuv::randomPort())
 golem::detach_all_attached()
 golem::document_and_reload()
 run_app()
+
+## Dev for individual vizualizations
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(reactable)
+  library(plotly)
+})
+
+conn <- nmdtx:::connect_db()
+
+anno <- function() {
+  conn %>% tbl("anno") %>% filter(gene_name == "SRSF2") %>% collect()
+}
+
+library(tidyverse)
+library(plotly)
+log10_or_max <- nmdtx:::log10_or_max
