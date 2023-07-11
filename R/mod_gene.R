4a82e076089b0355158fd859868f489f143e4ac0
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2023-07-11 14:33:48 +0200

    complete gene view

--- R/mod_gene.R
+++ R/mod_gene.R
@@ -1,3 +1,52 @@
+#' Plot Boxplot with Gene Highlight
+#'
+#' This function generates a boxplot with a highlighted gene based on the specified contrast.
+#'
+#' @param data A data frame containing the necessary columns: gene_name, contrasts, log2FoldChange.
+#' @param gene_name The name of the gene to highlight.
+#' @param xlims minimum, maximum values for x-axis
+#'
+#'
+#' @return A ggplot object displaying the boxplot with the highlighted gene.
+#'
+#' @examples
+# data <- data.frame(gene_name = c("Gene1", "Gene2", "Gene2"),
+#                    contrasts = c("Contrast1", "Contrast2", "Contrast2"),
+#                    log2FoldChange = c(1.5, -0.8, 2.2))
+# fc_boxplot(data, "Gene2", "Contrast2")
+#'
+#' @import dplyr
+#' @import ggplot2
+fc_boxplot <- function(data, index, gene_l2fc, xlims) {
+
+  data <- data[index, ]
+  gene_name <- data[[1, 'gene_name']]
+  contrast <- data[[1, 'contrasts']]
+  data <- mutate(data, y = 1, label = sprintf("↓ %s", gene_name))
+
+  gene_l2fc <- gene_l2fc %>% filter(contrasts == !!contrast)
+  text_color <- ifelse(data$log2FoldChange > 0, "red", "blue")
+  p <- gene_l2fc %>%
+    ggplot(aes(y = contrasts, x = log2FoldChange)) +
+    geom_boxplot() +
+    geom_text(
+      data = data,
+      aes(label = label, y = y),
+      position = position_nudge(y = 0.08),
+      hjust = 0,
+      colour = text_color,
+      size = 3.5
+    ) +
+    lims(x = xlims) +
+    labs(x = '', y = '') +
+    theme_minimal() +
+    theme(
+      axis.text.y=element_blank()
+    )
+  htmltools::plotTag(p, alt = 'plots')
+}
+
+
 #' gene view UI Function
 #'
 #' @description A shiny Module.
@@ -11,19 +60,8 @@
 mod_gene_ui <- function(id) {
   ns <- NS(id)
 
-  gene_view_grid <- create_grid(
-    areas = rbind(c("left", "right")),
-    cols_width = c("30%", "auto"),
-    rows_height = c("auto")
-  )
-
-  grid(
-    gene_view_grid,
-    left = reactableOutput(ns("gene_exp_table")) %>%
-      shinycssloaders::withSpinner(),
-    right = plotOutput(ns("gene_exp")) %>%
-      shinycssloaders::withSpinner()
-  )
+  reactableOutput(ns("gene_exp_table")) %>%
+    shinycssloaders::withSpinner()
 }
 
 #' gene view Server Functions
@@ -36,7 +74,7 @@ mod_gene_ui <- function(id) {
 #' @noRd
 mod_gene_server <- function(id, conn, gene_name, contrast) {
   moduleServer(id, function(input, output, session) {
-    data <- reactive({
+    dge <- reactive({
       conn %>%
         tbl("dge") %>%
         filter(contrasts %in% !!contrast) %>%
@@ -44,60 +82,81 @@ mod_gene_server <- function(id, conn, gene_name, contrast) {
         collect()
     })
 
+    gene_l2fc <- dge() %>%
+      select(gene_name, contrasts, log2FoldChange)
+
     output$gene_exp_table <- renderReactable({
-      validate(need(nrow(data()) > 0, "Gene not tested for DE."))
-      data() %>%
+      dge_nrow <- dge() %>%
         filter(gene_name == !!gene_name) %>%
-        mutate_at(vars(padj, log2FoldChange), ~ format(round(., digits = 2), nsmall = 2)) %>%
-        mutate(contrasts = fct_reorder(contrasts, nchar(contrast))) %>%
-        arrange(desc(contrasts)) %>%
-        reactable(
-          .,
+        nrow()
+      validate(need(dge_nrow > 0, "Gene not tested for DE."))
+
+      xmin <- gene_l2fc %>% pull(log2FoldChange) %>% min() * 1.1
+      xmax <- gene_l2fc %>% pull(log2FoldChange) %>% max() * 1.1
+
+      dge <- dge() %>%
+        filter(gene_name == !!gene_name) %>%
+        mutate(padj = padj %>% scales::scientific()) %>%
+        left_join(load_metadata(conn), by="contrasts")
+
+      reactable(
+          dge,
           highlight = TRUE,
           wrap = FALSE,
-          rowStyle = list(cursor = "pointer"),
+          details = function(index) fc_boxplot(dge, index, gene_l2fc, c(xmin, xmax)),
           theme = reactableTheme(
+            borderColor = "#dfe2e5",
             stripedColor = "#f6f8fa",
-            highlightColor = "#f0f5f9",
-            cellPadding = "8px 12px",
-            rowSelectedStyle = list(
-              backgroundColor = "#eee",
-              boxShadow = "inset 2px 0 0 0 #FF0000"
-            )
-          ),
+            highlightColor = "#FFFFBF",
+            cellPadding = "8px 12px"),
           columns = list(
-            contrast = colDef(
+
+            gene_name = colDef(
+              show = FALSE
+            ),
+            Knockdown = colDef(
+              show = FALSE
+            ),
+            cellline = colDef(
+              show = FALSE
+            ),
+            Knockout  = colDef(
+              show = FALSE
+            ),
+            name = colDef(
+              show = FALSE
+            ),
+            contrasts = colDef(
               width = 200,
+              show = TRUE,
+              html = TRUE,
+              cell = JS("
+    function(cellInfo) {
+    const kd = cellInfo.row['Knockdown']
+    const cl = cellInfo.row['cellline']
+    const ko = cellInfo.row['Knockout'] || 'NoKO'
+        return (
+          '<div>' +
+          '<strong>' + kd + '</strong> <br>' +
+          '<small><i>Cell-line</i>: ' + cl +
+          ';<i> KO</i>: ' + ko + ' </small></div>'
+        )
+      }")),
+            padj = colDef(
+              filterable = FALSE,
+              show = TRUE,
+              align = "right"
+            ),
+            log2FoldChange = colDef(
+              name = "log2fc",
+              show = TRUE,
+              format = colFormat(digits = 2),
+              filterable = FALSE
             )
           ),
-          defaultColDef = colDef(width = 80)
+          defaultColDef = colDef(
+            width = 100)
         )
-    })
-
-    output$gene_exp <- renderPlot({
-      gene_l2fc <- data() %>%
-        filter(gene_name == !!gene_name) %>%
-        dplyr::select(gene_name, contrasts, log2FoldChange) %>%
-        mutate(y = 0, yend = 0.7)
-
-      validate(need(nrow(gene_l2fc) > 0, "Gene not tested for DE."))
-      text_color <- ifelse(gene_l2fc$log2FoldChange > 0, "red", "blue")
-      label <- sprintf("↓ %s", gene_name)
-      data() %>%
-        mutate(contrasts = fct_reorder(contrasts, nchar(contrasts))) %>%
-        ggplot(aes(y = contrasts, x = log2FoldChange)) +
-        ylab("Density") +
-        geom_density_ridges(alpha = 0.5, color = NA, bandwidth = 0.083) +
-        theme_ridges() +
-        geom_text(
-          data = gene_l2fc,
-          aes(label = label),
-          position = position_nudge(y = 0.2),
-          hjust = 0,
-          colour = text_color,
-          size = 3.5
-        ) +
-        xlim(c(-2, 2))
     })
   })
 }
