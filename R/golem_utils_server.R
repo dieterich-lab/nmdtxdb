5dd55c698f360911c85cf627c7b851d3370be09d
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2023-07-12 11:20:12 +0200

    lint and trackhub functions

--- R/golem_utils_server.R
+++ R/golem_utils_server.R
@@ -100,22 +100,23 @@ log10_or_max <- function(x) {
   log10x
 }
 
-#' @importFrom httr modify_url GET http_type http_error status_code
+#' @importFrom httr modify_url GET http_type http_error status_code accept content
 #' @importFrom jsonlite fromJSON
 get_gene_info <- function(gene_id) {
+
   url <- modify_url(
     url = "https://mygene.info",
     path = paste0("v3/gene/", gene_id),
     query = "fields=symbol,summary,alias,uniprot,name"
   )
 
-  resp <- GET(url, httr::accept("application/json"))
+  resp <- GET(url, accept("application/json"))
 
   if (http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
 
-  parsed <- fromJSON(httr::content(resp, "text"), simplifyVector = TRUE)
+  parsed <- fromJSON(content(resp, "text"), simplifyVector = TRUE)
   if (http_error(resp)) {
     stop(
       sprintf(
@@ -144,19 +145,15 @@ get_gene_info <- function(gene_id) {
 render_gene_card <- function(gene_id, conn) {
   parsed <- get_gene_info(gene_id)$content
 
-  transcripts <- tbl(conn, "gtf") %>%
-    filter(type == "transcript", gene_id == !!gene_id) %>%
-    collect()
-
-  dge_l2fc <- tbl(conn, "dge") %>%
-    filter(gene_id == !!gene_id) %>%
-    collect() %>%
-    pull(log2FoldChange)
-  is_nmd <- transcripts %>% with(table(transcript_biotype == "nonsense_mediated_decay"))
-  n_transcripts <- nrow(transcripts)
-  n_nmd <- ifelse(length(is_nmd) == 2, is_nmd[2], 0)
-  has_support <- tbl(conn, "has_support") %>% collect()
-  n_support <- length(intersect(transcripts$transcript_id, has_support$transcript_id))
+  # dge_l2fc <- tbl(conn, "dge") %>%
+  #   filter(gene_id == !!gene_id) %>%
+  #   collect() %>%
+  #   pull(log2FoldChange)
+  # is_nmd <- transcripts %>% with(table(transcript_biotype == "nonsense_mediated_decay"))
+  # n_transcripts <- nrow(transcripts)
+  # n_nmd <- ifelse(length(is_nmd) == 2, is_nmd[2], 0)
+  # has_support <- tbl(conn, "has_support") %>% collect()
+  # n_support <- length(intersect(transcripts$transcript_id, has_support$transcript_id))
 
   card(
     align = "left",
@@ -174,21 +171,21 @@ render_gene_card <- function(gene_id, conn) {
           str_interp("Also known as: ${paste(parsed$alias, collapse=', ')}")
         )
       ),
-      div(str_glue("NMD isoforms: {n_nmd}/{sum(n_transcripts)}")),
-      div(
-        str_glue("Up-regulated in {sum(dge_l2fc > 0.1, na.rm=TRUE)}/{length(dge_l2fc)} cohorts"),
-        icon("question circle"),
-      ) %>% tagAppendAttributes(., "data-tooltip" = "Number of datasets with l2fc > 0.1"),
+      # div(str_glue("NMD isoforms: {n_nmd}/{sum(n_transcripts)}")),
+      # div(
+      #   str_glue("Up-regulated in {sum(dge_l2fc > 0.1, na.rm=TRUE)}/{length(dge_l2fc)} cohorts"),
+      #   icon("question circle"),
+      # ) %>% tagAppendAttributes(., "data-tooltip" = "Number of datasets with l2fc > 0.1"),
       # p("Novel transcripts : X/XX"),
+      # div(
+      #   str_glue("Long read evidence: {n_support}/{n_transcripts}"),
+      #   icon("question circle"),
+      # ) %>% tagAppendAttributes(
+      #   .,
+      #   "data-tooltip" = "Isoforms with identical intron chain"
+      # ),
       div(
-        str_glue("Long read evidence: {n_support}/{n_transcripts}"),
-        icon("question circle"),
-      ) %>% tagAppendAttributes(
-        .,
-        "data-tooltip" = "Isoforms with identical intron chain"
-      ),
-      div(
-        "Ensembl: ",
+        "Ensembl:",
         a(
           gene_id,
           href = paste0("https://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=", gene_id),
@@ -198,7 +195,7 @@ render_gene_card <- function(gene_id, conn) {
       conditionalPanel(
         !is.null(parsed$uniprot$`Swiss-Prot`),
         p(
-          "Uniprot: ",
+          "Uniprot:",
           a(
             parsed$uniprot$`Swiss-Prot`,
             href = paste0("https://www.uniprot.org/uniprotkb/", parsed$uniprot$`Swiss-Prot`),
@@ -279,7 +276,6 @@ connect_db <- function(
 #' Simplifies grid creation
 #' @note does not support mobile grid.
 #' @importFrom shiny.semantic grid_template
-#' @export
 create_grid <- function(areas, cols_width, rows_height) {
   shiny.semantic::grid_template(
     default = list(
@@ -288,4 +284,82 @@ create_grid <- function(areas, cols_width, rows_height) {
       rows_height = rows_height
     )
   )
+}
+
+#' Extract Genomic Position from GTF
+#'
+#' This function extracts the genomic position from a GTF file by grouping the data based on a specified column and summarizing the sequence name, start, and end coordinates.
+#'
+#' @param gtf A data frame containing the GTF data.
+#' @param col The column to group by for summarization.
+#'
+#' @return A character string representing the genomic position in the format "chr{seqnames}:{start}-{end}".
+#'
+#' @examples
+#' gtf <- data.frame(seqnames = c("chr1", "chr1", "chr2", "chr2"),
+#'                   start = c(100, 200, 300, 400),
+#'                   end = c(500, 600, 700, 800),
+#'                   gene_id = c("Gene1", "Gene2", "Gene3", "Gene4"))
+#' position_from_gtf(gtf, "gene_id")
+#'
+#' @importFrom dplyr group_by summarise first
+#' @importFrom stringr str_glue_data
+#'
+position_from_gtf <- function(gtf, col = 'gene_id') {
+  gtf %>%
+    group_by(!!col) %>%
+    summarise(
+      seqnames = first(seqnames),
+      start = min(start,na.rm = TRUE),
+      end = max(end)
+    ) %>%
+    mutate(chr = ifelse(str_detect(seqnames, 'chr'), '', 'chr')) %>%
+    str_glue_data("{chr}{seqnames}:{start}-{end}")
+}
+
+
+#' Create Trackhub URL
+#'
+#' This function generates a URL for accessing a track hub on the UCSC Genome Browser.
+#'
+#' @param base_url The base URL of the UCSC Genome Browser. Default is "http://genome-euro.ucsc.edu/".
+#' @param db The genome database. Default is "hg38".
+#' @param position The genomic position. Default is NA for no posiitoj.
+#' @param hub The URL of the track hub. Default is "https://trackhub.dieterichlab.org/tbrittoborges/nmd_transcriptome/nmd_transcriptome.hub.txt".
+#'
+#' @return A URL for accessing the specified track hub on the UCSC Genome Browser.
+#'
+#' @examples
+#' create_trackhub_url()
+#' create_trackhub_url(db = "mm10", position = "chr1:1000-2000")
+#' create_trackhub_url(db = "mm10", position = "chr1:1000-2000", hub=NA)
+#' @importFrom httr modify_url
+#'
+create_trackhub_url <- function(
+    base_url="http://genome-euro.ucsc.edu/", db="hg38", position=NA, hub="https://trackhub.dieterichlab.org/tbrittoborges/nmd_transcriptome/nmd_transcriptome.hub.txt") {
+  query <- list(
+    db = db,
+    position = position,
+    hubUrl = hub)
+
+  modify_url(
+    url = base_url,
+    path = "cgi-bin/hgTracks",
+    query = query[!is.na(query)]
+  )
+}
+
+load_metadata <- function(conn) {
+  conn %>%
+    tbl("metadata") %>%
+    select(contrasts, Knockdown, Knockout, cellline) %>%
+    filter(Knockdown != "LucKD") %>%
+    distinct() %>%
+    collect() %>%
+    mutate(
+      name = str_replace(contrasts, "-vs-.*", ""),
+      Knockout = str_replace(Knockout, "_", "")
+    )
+
+
 }
