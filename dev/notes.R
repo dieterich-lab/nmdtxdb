d22e7265c0e13a4d7e6572e303fa848d5809e1d7
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2023-11-15 14:12:50 +0100

    fix dettached

--- dev/notes.R
+++ dev/notes.R
@@ -5,21 +5,12 @@ suppressPackageStartupMessages({
   library(plotly)
 })
 
-db <- readRDS("database2.RDS")
+# db <- readRDS("database2.RDS")
 
 anno <- db[['anno']] %>% filter(gene_name == "SRSF2")
 contrast <- c("HEK_SMG7KO_SMG6KD-KD_Z319-vs-HEK_SMG7KO_LucKD-KD_Z319")
 tx <- anno$transcript_id
-
-
-from_start <- readRDS('from_start.RDS')
-from_start$thick <- as.character(from_start$thick)
-from_start$cdna_thick %>% head()
-from_start$is_ptc
-from_start[c("blocks",  "cdna_thick.start", "cdna_thick.end", "cdna_thick.width", "itemRgb")] <- NUL
-
-
-
+cds <- c('canonical', 'openprot')
 
 library(tidyverse)
 library(plotly)
