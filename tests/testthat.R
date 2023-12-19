4729a920e33387b7b9a5c3db2bf8ef79a7cd803e
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2023-12-19 10:34:01 +0100

    add more test for gene card

--- tests/testthat.R
+++ tests/testthat.R
@@ -14,8 +14,31 @@ test_check("nmdtx")
 context("render_gene_card")
 
 test_that("render_gene_card returns a valid HTML character", {
-  output <- nmdtx:::render_gene_card('ENSG00000160201')
+  output <- nmdtx:::render_gene_card("ENSG00000160201")
   expect_is(output, "character",
-            info = "The output of render_gene_card should be a character string")
+    info = "The output of render_gene_card should be a character string"
+  )
+})
 
+# Assuming you have a valid gene_id for testing
+valid_gene_id <- "ENSG00000160201"
+
+# Test 1: Function should not throw an error with valid input
+test_that("Function does not error with valid input", {
+  expect_silent(render_gene_card(valid_gene_id))
+})
+
+# Test 2: Function should return valid HTML
+test_that("Function returns valid HTML", {
+  output <- render_gene_card(valid_gene_id)
+  expect_true(grepl("<div", output)) # Simple check for HTML tag
+  # Additional checks can be added to ensure the structure of the HTML is as expected
+})
+
+# Test 3: Function should return a helpful error for invalid input
+test_that("Function returns helpful error for invalid input", {
+  invalid_gene_id <- "ABC123"
+  expect_error(render_gene_card(invalid_gene_id),
+    regexp = "Input should start with 'ENS'"
+  )
 })
