09104e271b6d215ca4e406b33d2341a8eaf4497a
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2024-07-03 15:59:25 +0200

    fix install.packages not installing.

--- deploy/launch_shiny_app.sh
+++ deploy/launch_shiny_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export RENV_WATCHDOG_ENABLED=0
 
-Rscript -e 'renv::restore(); install.packages("app.tar.gz", repos = NULL, type ="source"); renv::isolate()'
+Rscript -e 'renv::restore(); install.packages("app.tar.gz", source=TRUE); renv::isolate()'
 
 echo "Running renv $(pidof  R)"
 setsid R -f rstart.R > /app/rlogs.log 2>&1 &
