887cbf36306ed1b5548ba73404ab889fc9249854
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2024-07-03 11:57:53 +0200

    lock missing few repos

--- deploy/launch_shiny_app.sh
+++ deploy/launch_shiny_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export RENV_WATCHDOG_ENABLED=0
 
-Rscript -e 'renv::restore(); renv::install("markdown", "plotly", "reshape2", "shinyjs", "tidyverse"); install.packages("app.tar.gz", repos = NULL, type ="source"); renv::isolate()'
+Rscript -e 'renv::restore(); install.packages("app.tar.gz", repos = NULL, type ="source"); renv::isolate()'
 
 echo "Running renv $(pidof  R)"
 setsid R -f rstart.R > /app/rlogs.log 2>&1 &
